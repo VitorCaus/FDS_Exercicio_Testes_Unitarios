4429ad3e1b93126dcaa15fba9449f19dc111ac4b
VitorCaus <vitor50033@outlook.com>
2023-10-01 14:27:39 -0300

    Testes baseados em partição e em valor limite adicionados

--- src/test/java/BarcaTest.java
+++ src/test/java/BarcaTest.java
@@ -12,91 +12,259 @@ public class BarcaTest {
         barca = new Barca();
     }
 
-
-    @Test
-    public void testeFilaUmDIgito(){
-        String assento = "F2A03";
+    @Test 
+    public void testePartValido100Primeiros(){
+        String assento = "F14A07";
         int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(0, retorno);
-    }
-    
-    @Test
-    public void testeAssentoUmDIgito(){
-        String assento = "F32A3";
-        int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(0, retorno);
+        Assertions.assertEquals(3, retorno);
     }
 
-    @Test
-    public void testeFilaNula(){
-        String assento = "F00A03";
-        int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(0, retorno);
-    }
 
-    @Test
-    public void testeAssentoNulo(){
-        String assento = "F50A00";
-        int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(0, retorno);
-    }
-
-    @Test
-    public void testeFilaMaior60(){
-        String assento = "F75A14";
-        int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(0, retorno);
-    }
-
-    @Test
-    public void testeAssentoMaior20(){
-        String assento = "F25A21";
-        int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(0, retorno);
-    }
-
-    @Test
-    public void testeOcuparJaOcupado(){
-        String assento = "F25A02";
-        barca.ocupaLugarSemVerificacao(25, 2);
-        int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(1, retorno);
-    }
-
-    @Test
-    public void testePesoPrimeiros100(){
-        for(int i = 1; i<=20; i++){//ocupando 80 assentos no inicio
-            for(int j = 1; j <=4; j++){
-                barca.ocupaLugarSemVerificacao(i, j);
-            }
-        }
-
-        String assento = "F40A19";
+    @Test 
+    public void testePartBlockPeso100Primeiros(){
+        String assento = "F44A15";
         int retorno = barca.ocupaLugar(assento);
         Assertions.assertEquals(2, retorno);
     }
 
-    @Test
-    public void testePesoPrimeiros200(){
+
+    @Test 
+    public void testePartValido100Proximos(){
         for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
             for(int j = 1; j <=5; j++){
                 barca.ocupaLugarSemVerificacao(i, j);
             }
         }
+        String assento = "F50A03";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(3, retorno);
+    }
 
-        for(int i = 40; i<60; i++){//ocupando 80 assentos no fim
+
+    @Test 
+    public void testePartBlockPeso100Proximos(){
+        for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
             for(int j = 1; j <=4; j++){
                 barca.ocupaLugarSemVerificacao(i, j);
             }
         }
-
         String assento = "F30A10";
         int retorno = barca.ocupaLugar(assento);
         Assertions.assertEquals(2, retorno);
     }
 
+
+    @Test 
+    public void testePartValidoPesoEquilibrado(){
+        for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
+            for(int j = 1; j <=5; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        for(int i = 40; i<60; i++){//ocupando 100 assentos no fim
+            for(int j = 1; j <=5; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F29A13";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(3, retorno);
+    }
+
+    @Test 
+    public void testePartLugarJaOcupado(){
+        
+        String assento = "F10A10 ";
+        barca.ocupaLugarSemVerificacao(10, 10);
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(1, retorno);
+    }
+
+
+    @Test 
+    public void testePartFilaNula(){
+        String assento = "F00A11 ";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+    }
+
+
+    @Test 
+    public void testePartAssentoNulo(){
+        String assento = "F19A00 ";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+    }
+
+
+    @Test 
+    public void testePartFila1Digito(){
+        String assento = "F3A05 ";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+    }
+
+
+    @Test 
+    public void testePartAssento1Digito(){
+        String assento = "F12A9 ";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+    }
+
+
+    @Test 
+    public void testePartFilaAcimaLimite(){
+        for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
+            for(int j = 1; j <=5; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F80A10 ";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+    }
+
+
+    @Test 
+    public void testePartAssentoAcimaLimite(){
+        String assento = "F16A31 ";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+    }
+
+/*
+ * 
+ *  TESTES VALOR LIMITE
+ * 
+ */
     @Test
-    public void testePesoJaDistribuido(){
+    public void testeVLFilaNula(){
+        String assento = "F00A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+    }
+
+    @Test
+    public void testeVLFilaValida(){
+        String assento = "F01A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(3, retorno);
+
+    }
+
+    @Test
+    public void testeVLAssentoOcupado(){
+        String assento = "F10A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(1, retorno);
+
+    }
+
+    @Test
+    public void testeVLFilaNegativa(){
+        String assento = "F-5A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+
+    }
+
+    @Test
+    public void testeVLFilaValidaLimite20(){
+        String assento = "F20A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(3, retorno);
+
+    }
+
+    @Test
+    public void testeVLBlockPeso100PrimeirosOffP(){
+        for(int i = 1; i<=20; i++){//ocupando 80 assentos no inicio
+            for(int j = 1; j <=4; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F21A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(2, retorno);
+
+    }
+
+    @Test
+    public void testeVLBlockPeso100PrimeirosOutP(){
+        for(int i = 1; i<=20; i++){//ocupando 80 assentos no inicio
+            for(int j = 1; j <=4; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F30A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(2, retorno);
+
+    }
+
+    @Test
+    public void testeVLValido200Primeiros(){
+        for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
+            for(int j = 1; j <=5; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F60A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(3, retorno);
+
+    }
+
+    @Test
+    public void testeVLFilaAcima60OffP(){
+        String assento = "F61A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+
+    }
+
+
+    @Test
+    public void testeVLFilaAcima60OutP(){
+        String assento = "F70A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+
+    }
+
+    @Test
+    public void testeVLAssentoNulo(){
+        String assento = "F08A00";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+
+    }
+
+    @Test
+    public void testeVLBlockPeso200Primeiros(){
+        for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
+            for(int j = 1; j <=5; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F09A10";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(2, retorno);
+
+    }
+
+
+    @Test
+    public void testeVLAssentoNegativo(){
+        String assento = "F14A-1";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+
+    }
+
+    @Test
+    public void testeVLOcupadoDemaisPass(){
         for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
             for(int j = 1; j <=5; j++){
                 barca.ocupaLugarSemVerificacao(i, j);
@@ -108,9 +276,38 @@ public class BarcaTest {
                 barca.ocupaLugarSemVerificacao(i, j);
             }
         }
+        barca.ocupaLugarSemVerificacao(45, 20);
 
-        String assento = "F30A13";
+        String assento = "F45A20";
         int retorno = barca.ocupaLugar(assento);
-        Assertions.assertEquals(3, retorno);
+        Assertions.assertEquals(1, retorno);
+
     }
+
+    @Test
+    public void testeVLAssentoAcima20OffP(){
+        for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
+            for(int j = 1; j <=5; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F50A21";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+
+    }
+
+    @Test
+    public void testeVLAssentoAcima20OutP(){
+        for(int i = 1; i<=20; i++){//ocupando 100 assentos no inicio
+            for(int j = 1; j <=5; j++){
+                barca.ocupaLugarSemVerificacao(i, j);
+            }
+        }
+        String assento = "F55A25";
+        int retorno = barca.ocupaLugar(assento);
+        Assertions.assertEquals(0, retorno);
+
+    }
+    
 }
